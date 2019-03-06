561624857d084b5d075e607721ef920fc15e1f9e
Critonik <42637808+Critonik@users.noreply.github.com>
2019-03-06 23:24:42 +0500

    JS

--- js/script.js
+++ js/script.js
@@ -1,12 +1,20 @@
 var link = document.querySelector(".find");
 var popup = document.querySelector(".appointment-form");
-link.addEventListener("click", fuction (evt) {
+link.addEventListener("click", function (evt) {
   evt.preventDefault();
-  if (popup.classList.contains("appointment-form-close"))
+  if (!popup.classList.contains("appointment-form-close"))
     {
-      popup.classList.remove("appointment-form-close");
-    }
-    else {
       popup.classList.add("appointment-form-close");
     }
+    else {
+      popup.classList.remove("appointment-form-close");
+    }
+});
+window.addEventListener("keydown", function (evt) {
+  if (evt.keyCode === 27) {
+    evt.preventDefault();
+    if (popup.classList.contains("appointment-form-close")) {
+      popup.classList.remove("appointment-form-close");
+    }
+  }
 });
