4ce39bbdf1cc6a3ecaf4b63c846a311336749f7f
Critonik <42637808+Critonik@users.noreply.github.com>
2019-03-11 22:10:50 +0500

    фикс жс

--- js/script.js
+++ js/script.js
@@ -5,6 +5,23 @@ var departuredate = popup.querySelector("[name=departuredate]");
 var childcount = popup.querySelector("[name=childcount]");
 var adultcount = popup.querySelector("[name=adultcount]");
 var form = document.querySelector("form");
+var isStorageSupport = true;
+var storage = "";
+
+try {
+    storage = localStorage.getItem("adultcount");
+  }
+catch (err) {
+    isStorageSupport = false;
+  }
+
+if (storage) {
+  adultcount.value = storage;
+  }
+  else {
+  appointmentdate.focus();
+  }
+
 
 link.addEventListener("click", function (evt) {
   evt.preventDefault();
@@ -12,6 +29,7 @@ link.addEventListener("click", function (evt) {
     {
       popup.classList.add("appointment-form-open");
       appointmentdate.focus();
+      popup.classList.remove("appointment-form-error");
     }
     else {
       popup.classList.remove("appointment-form-open");
@@ -30,15 +48,17 @@ window.addEventListener("keydown", function (evt) {
 });
 
 form.addEventListener("submit", function (evt) {
-    evt.preventDefault();
     if (!appointmentdate.value || !departuredate.value || !adultcount.value || !childcount.value) {
       evt.preventDefault();
-      popup.classList.add("appointment-form-error");
+      popup.classList.remove  ("appointment-form-error");
       popup.offsetWidth = popup.offsetWidth;
       popup.classList.add("appointment-form-error");
       console.log("Нужно ввести дату прибытия и отбытия");
     }
     else {
       popup.classList.remove("appointment-form-error");
+      if (isStorageSupport) {
+        localStorage.setItem("adultcount", adultcount.value);
+      }
     }
   });
